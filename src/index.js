c73883fc239f0f0293f8ff2a80bdc85ffef686b5
Theo Rijksen <theor@HP-THEO.solidbe.local>
2018-06-13 11:34:16 +0200

    Add base for component search_bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyCqdPJVhzdQZrW-tT333YhMjGmVOa9VnJE';
 
-const App = () => {
-    return <div>Hi!</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            //this.setState({ videos: videos });
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
